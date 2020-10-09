462da03e530ded46ac9af736fb67e680a1184dce
Andrei Ilin <ortfero@gmail.com>
2020-10-09 20:13:27 +0300

    Initialize winsock

--- include/telegraf/telegraf.hpp
+++ include/telegraf/telegraf.hpp
@@ -1,16 +1,63 @@
 #pragma once
 
 
+#if defined(_WIN32)
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
+
+
 namespace telegraf {
+
+
+class context {
+public:
+
+  context() noexcept {
+#if defined(_WIN32)
+    auto const code = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    ready = (code == 0);
+#endif
+  }
+
+  ~context() {
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+  }
+
+  explicit operator bool() const noexcept {
+#if defined(_WIN32)
+    return ready;
+#else
+    return true;
+#endif
+  }
+
+private:
+
+#if defined(_WIN32)
+  WSADATA wsadata;
+  bool ready;
+#endif
+};
+
 
 class bot {
 public:
 
-  bot() noexcept = default;
+  bot(context&) noexcept { }
   bot(bot const&) = delete;
   bot& operator = (bot const&) = delete;
   bot(bot&&) noexcept = default;
   bot& operator = (bot&&) noexcept = default;
+
+private:
+
+#if defined(_WIN32)
+  SOCKET socket{INVALID_SOCKET};
+#endif
+
 };
 
 
